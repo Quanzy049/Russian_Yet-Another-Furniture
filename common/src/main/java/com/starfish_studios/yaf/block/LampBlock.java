f7ea3a2a26bfd1e8d24f06c46f0bec488e60a843
mrsterner <simon@sterner.dev>
2025-06-16 08:03:15 +0200

    1.21.1 fix
    # Conflicts:
    #	common/src/main/java/com/starfish_studios/yaf/block/LampBlock.java

--- common/src/main/java/com/starfish_studios/yaf/block/LampBlock.java
+++ common/src/main/java/com/starfish_studios/yaf/block/LampBlock.java
@@ -1,6 +1,5 @@
 package com.starfish_studios.yaf.block;
 
-import com.starfish_studios.yaf.block.entity.LampBlockEntity;
 import com.starfish_studios.yaf.block.properties.ColorList;
 import com.starfish_studios.yaf.registry.YAFSoundEvents;
 import net.minecraft.core.BlockPos;
@@ -20,7 +19,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -31,9 +29,8 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
 
-public class LampBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
+public class LampBlock extends Block implements SimpleWaterloggedBlock {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final BooleanProperty LIT = BlockStateProperties.LIT;
     public static final EnumProperty<LampType> LAMP_TYPE = EnumProperty.create("lamp_type", LampType.class);
@@ -65,6 +62,7 @@ public class LampBlock extends BaseEntityBlock implements SimpleWaterloggedBlock
         };
     }
 
+    @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         if (!level.isClientSide) {
             ItemStack heldItem = player.getItemInHand(hand);
@@ -166,11 +164,6 @@ public class LampBlock extends BaseEntityBlock implements SimpleWaterloggedBlock
             case MIDDLE, TOP -> belowIsLamp;
             case BOTTOM -> aboveIsLamp;
         };
-    }
-
-    @Override
-    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new LampBlockEntity(pos, state);
     }
 
     public enum LampType implements StringRepresentable {
